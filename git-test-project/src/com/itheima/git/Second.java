dff378c3b6da86d239e4a6267361793f14bec108
shyu_leung <shyu_lueng@163.com>
2021-05-12 00:16:22 +0800

    2次修改second类

--- git-test-project/src/com/itheima/git/Second.java
+++ git-test-project/src/com/itheima/git/Second.java
@@ -4,5 +4,6 @@ public class Second {
     public static void main(String[] args) {
         System.out.println("5555555555555555555555");
         System.out.println("6666666666666666666666666666666");
+        System.out.println("7777777777777777777777777777777777777");
     }
 }
