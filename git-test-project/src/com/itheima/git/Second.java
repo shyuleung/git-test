11cf5291d09c53859bed22d7696e7b75419775f4
shyu_leung <shyu_lueng@163.com>
2021-05-12 00:38:06 +0800

    88888888888888

--- git-test-project/src/com/itheima/git/Second.java
+++ git-test-project/src/com/itheima/git/Second.java
@@ -3,7 +3,7 @@ package com.itheima.git;
 public class Second {
     public static void main(String[] args) {
         System.out.println("5555555555555555555555");
-        System.out.println("6666666666666666666666666666666");
+        System.out.println("666666666666666666666666666666688888888888888888888888888");
         System.out.println("7777777777777777777777777777777777777");
     }
 }
